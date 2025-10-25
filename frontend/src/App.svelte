8038f9436d9bd2e2d8c69680c575a05e117e210d
Kerry Meng-Lin <kmenglin@google.com>
2025-10-25 05:49:48 +0000

    Add basic functionality for adding and deleting nodes via UI

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,79 +1,42 @@
 <script lang="ts">
-  import logo from './assets/images/logo-universal.png'
-  import {Greet} from '../wailsjs/go/app/App.js'
+  import { onMount } from 'svelte';
+  import { GetNodes, AddNode, DeleteNode } from '../wailsjs/go/app/App';
 
-  let resultText: string = "Please enter your name below 👇"
-  let name: string
-
-  function greet(): void {
-    Greet(name).then(result => resultText = result)
+  interface Node {
+    X: number;
+    Y: number;
   }
+
+  let nodes: Node[] = [];
+
+  async function loadNodes() {
+    nodes = await GetNodes();
+  }
+
+  async function addNode() {
+    nodes = await AddNode();
+  }
+
+  async function deleteNode() {
+    nodes = await DeleteNode();
+  }
+
+  onMount(loadNodes);
 </script>
 
 <main>
-  <img alt="Wails logo" id="logo" src="{logo}">
-  <div class="result" id="result">{resultText}</div>
-  <div class="input-box" id="input">
-    <input autocomplete="off" bind:value={name} class="input" id="name" type="text"/>
-    <button class="btn" on:click={greet}>Greet</button>
-  </div>
+  <button on:click={addNode}> Add Node</button>
+  <button on:click={deleteNode}> Delete Node</button>
+  <svg width="100%" height="500">
+    {#each nodes as node}
+      <circle cx={node.X} cy={node.Y} r="10" fill="lightblue"/>
+    {/each}
+  </svg>
+
 </main>
 
 <style>
-
-  #logo {
-    display: block;
-    width: 50%;
-    height: 50%;
-    margin: auto;
-    padding: 10% 0 0;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: 100% 100%;
-    background-origin: content-box;
+  svg {
+    border: 1px solid #fff;
   }
-
-  .result {
-    height: 20px;
-    line-height: 20px;
-    margin: 1.5rem auto;
-  }
-
-  .input-box .btn {
-    width: 60px;
-    height: 30px;
-    line-height: 30px;
-    border-radius: 3px;
-    border: none;
-    margin: 0 0 0 20px;
-    padding: 0 8px;
-    cursor: pointer;
-  }
-
-  .input-box .btn:hover {
-    background-image: linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%);
-    color: #333333;
-  }
-
-  .input-box .input {
-    border: none;
-    border-radius: 3px;
-    outline: none;
-    height: 30px;
-    line-height: 30px;
-    padding: 0 10px;
-    background-color: rgba(240, 240, 240, 1);
-    -webkit-font-smoothing: antialiased;
-  }
-
-  .input-box .input:hover {
-    border: none;
-    background-color: rgba(255, 255, 255, 1);
-  }
-
-  .input-box .input:focus {
-    border: none;
-    background-color: rgba(255, 255, 255, 1);
-  }
-
 </style>
---
+++ frontend/wailsjs/go/models.ts
@@ -0,0 +1,19 @@
+export namespace model {
+	
+	export class Node {
+	    X: number;
+	    Y: number;
+	
+	    static createFrom(source: any = {}) {
+	        return new Node(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.X = source["X"];
+	        this.Y = source["Y"];
+	    }
+	}
+
+}
+
